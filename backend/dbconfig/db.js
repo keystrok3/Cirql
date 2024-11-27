a3381a2b1c54a81d9125566aa08b82b4d1c810f2
Josiah Mukhwana <jwalutsachi4@gmail.com>
2024-11-27 19:39:36 +0300

    chore: Add sequelize instance creation and db connection code.

---
+++ backend/dbconfig/db.js
@@ -0,0 +1,32 @@
+const { Sequelize } = require("sequelize");
+
+
+const db_connection = new Sequelize(
+    {
+        database: process.env.DATABASE_NAME,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD
+    },
+    {
+        dialect: 'postgres',
+        host: 'localhost',
+        logging: console.log
+    }
+)
+
+async function connect_to_db() {
+    try {
+        await db_connection.authenticate()
+        console.log('Connected to database ', process.env.DATABASE_NAME)
+    } catch (error) {
+        console.error(`
+            \nCould not connect to database ${process.env.DATABASE_NAME} \n
+            Error: ${error}
+        `)
+    }
+}
+
+module.exports = {
+    db_connection,
+    connect_to_db
+}
