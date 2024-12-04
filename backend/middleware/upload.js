15b615cdfc0fafc9d2e454d466a8a668738a9a88
Josiah Mukhwana <jwalutsachi4@gmail.com>
2024-12-04 22:15:53 +0300

    Chore: move multer functions to middleware/

---
+++ backend/middleware/upload.js
@@ -0,0 +1,39 @@
+const multer = require('multer')
+const path = require('path')
+
+// configure multer storage
+const storage = multer.diskStorage({
+    // define destination directory
+    destination: function(req, file, cb) {
+        cb(null, 'public/photouploads')
+    },
+
+    // define filename for uploaded files
+    filename:   function(req, file, cb) {
+        // generate unique filename using timestamp and extension
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname))
+    }
+})
+
+// create filter to allow only images
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype.startsWith('image/')) {
+        cb(null, true)
+    } else {
+        cb(new Error('Not an image! Please upload only images.'), false)
+    }
+}
+
+
+// Initialize multer with above configs
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 10 * 1024 * 1024
+    }
+})
+
+
+module.exports = { upload }
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 require('dotenv').config({ path: './config.env' })
 
 const express = require('express')
-const multer = require('multer')
-const path = require('path')
 const { connect_to_db } = require('./dbconfig/db')
 const { initialize_models } = require('./models/init_models')
 const { errorHandler } = require('./middleware/error')
@@ -20,40 +18,6 @@ app.use('/images', express.static('public/photouploads'))
 app.use('/api/auth', require('./routes/auth'))
 app.use(errorHandler)
 
-
-// configure multer storage
-const storage = multer.diskStorage({
-    // define destination directory
-    destination: function(req, file, cb) {
-        cb(null, 'public/photouploads')
-    },
-
-    // define filename for uploaded files
-    filename:   function(req, file, cb) {
-        // generate unique filename using timestamp and extension
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname))
-    }
-})
-
-// create filter to allow only images
-const fileFilter = (req, file, cb) => {
-    if(file.mimetype.startsWith('image/')) {
-        cb(null, true)
-    } else {
-        cb(new Error('Not an image! Please upload only images.'), false)
-    }
-}
-
-
-// Initialize multer with above configs
-const upload = multer({
-    storage: storage,
-    fileFilter: fileFilter,
-    limits: {
-        fileSize: 10 * 1024 * 1024
-    }
-})
 
 
 const server = app.listen(PORT, () => {
