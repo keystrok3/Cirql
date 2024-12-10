d9f07e1a48071b402026800a2065918b21f835b3
Josiah Mukhwana <jwalutsachi4@gmail.com>
2024-12-10 20:40:07 +0300

    Add: theme spec for primary colors and background color

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ThemeProvider from '@mui/material/styles/ThemeProvider'
 import './index.css'
 import App from './App.jsx'
+import theme from './theme/theme.js'
+import { CssBaseline } from '@mui/material'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
---
+++ frontend/src/theme/theme.js
@@ -0,0 +1,17 @@
+import createTheme from '@mui/material/styles/createTheme'
+
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#4A90E2"
+        },
+
+        background: {
+            default: "#F9F9F9",
+            paper: "#F2F4F6",
+        }
+    }
+})
+
+export default theme;
